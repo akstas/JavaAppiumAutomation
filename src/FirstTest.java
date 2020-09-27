b95dbfca3e80484dab78b2b1062d9dc3d76c9534
Stanislav Mslv <akstas@Mac-mini-Stanislav.local>
2020-09-27 12:25:19 +0300

    add test Ex2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,11 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class FirstTest extends BaseTest{
 
@@ -108,6 +110,7 @@ public class FirstTest extends BaseTest{
                 "Cannot find titles id",
                 15
         );
+
         String articleTitle = titleElement.getAttribute("contentDescription");
         String s = articleTitle;
 
@@ -116,8 +119,44 @@ public class FirstTest extends BaseTest{
                 "General-purpose programming language",
                 articleTitle
         );
+    }
+    
+    @Test
+    public void Ex2()
+    {
+        waitForElementPresentAndClick(
+                By.xpath("//*[contains(@text, 'SKIP')]"),
+                "Cannot find SKIP button",
+                5
+        );
+        waitForElementPresentAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia fild",
+                5
+        );
+        waitForElementPresentByIdAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Android Kotlin",
+                "Cannot find search input",
+                5
+        );
 
-
+        assertElementHasText(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Android Kotlin",
+                "The search string does not contain the expected text "
+        );
+    }
+    private void assertElementHasText(By by, String expectedText, String errorMessage) {
+        WebElement element = waitForElementPresent(by, errorMessage, 5);
+        String actualTitle = element.getAttribute("text");
+        Assert.assertEquals(errorMessage, expectedText, actualTitle);
+    }
+    private  WebElement waitForElementPresent(By by, String errorMessage, long timeOutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
     private  WebElement waitForElementPresentAndClick(By by, String errorMessage, long timeOutInSeconds)
@@ -132,12 +171,7 @@ public class FirstTest extends BaseTest{
         element.sendKeys(value);
         return element;
     }
-    private  WebElement waitForElementPresent(By by, String errorMessage, long timeOutInSeconds)
-    {
-        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
-        wait.withMessage(errorMessage + "\n");
-        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
-    }
+
     private boolean waitForElementNotPresent(By by, String errorMessage, long timeOutInMessage)
     {
         WebDriverWait wait = new WebDriverWait(driver, timeOutInMessage);
