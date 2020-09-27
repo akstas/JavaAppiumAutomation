53c191af84c9a736c26945f1f3cb9fff149a5fae
Stanislav Mslv <akstas@Mac-mini-Stanislav.local>
2020-09-27 14:26:31 +0300

    add test Ex3 testSerchListEx3

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -4,8 +4,45 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
 
 public class FirstTest extends BaseTest{
+
+    @Test
+    public void testSerchListEx3()
+    {
+        waitForElementPresentAndClick(
+                By.xpath("//*[contains(@text, 'SKIP')]"),
+                "Cannot find SKIP button",
+                5
+        );
+        waitForElementPresentAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia fild",
+                5
+        );
+        waitForElementPresentByIdAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Android",
+                "Cannot find search input",
+                5
+        );
+
+        By elementId = By.id("org.wikipedia:id/search_results_list");
+        Assert.assertTrue("Item not in the list",searchResult(elementId) > 0);
+        waitForElementPresentAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "",
+                5
+        );
+
+        Assert.assertTrue("After canceling the search, articles are still on the list", searchResult(elementId) == 0);
+    }
+    public int searchResult(By by)
+    {
+        List<WebElement> elementsList1 = driver.findElements(by);
+        return elementsList1.size();
+    }
 
     @Test
     public void firstTest()
